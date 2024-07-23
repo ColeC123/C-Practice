e56805d81ebf6e8858395bcf63a42f4d08860641
Cole Chapman <colechapman924@gmail.com>
2024-07-23 15:49:11 -0700

    added function to get char from string

--- test.c
+++ test.c
@@ -14,12 +14,18 @@ typedef struct string
 
 void set_string(string *str, char *text);
 
+char get_char(string* str, int index);
+
 int main(void)
 {
     string str = default_string;
     set_string(&str, "Hello World");
     printf("%s\n", str.text);
     printf("%d\n", str.length);
+    set_string(&str, "This is a program");
+    printf("%s\n", str.text);
+    printf("%d\n", str.length);
+    printf("%c\n", get_char(&str, 17));
     free(str.text);
     return 0;
 }
@@ -66,5 +72,20 @@ void set_string(string *str, char *text)
         text -= str->length;
         *(str->text) = '\0';
         str->text -= str->length;
+    }
+}
+
+char get_char(string* str, int index) {
+    char result;
+    if (index < str->length) {
+        for (int i = 0; i < index; i++) {
+            str->text++;
+        }
+        result = *str->text;
+        str->text -= index;
+        return result;
+    } else {
+        printf("--> Index out of bounds error. Tried to access an element beyond the length of the string <--");
+        exit(1);
     }
 }
