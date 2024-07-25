fc58e3acc026f87b8d0085e67fdd1bf72f7c90be
Cole Chapman <colechapman924@gmail.com>
2024-07-24 20:37:11 -0700

    Feat: Added "destroy_string" function and added handling for invalid inputs into the "set_string" function

--- test.c
+++ test.c
@@ -16,6 +16,8 @@ void set_string(string *str, char *text);
 
 char get_char(string* str, int index);
 
+void destroy_string(string* str);
+
 int main(void)
 {
     string str = default_string;
@@ -25,34 +27,14 @@ int main(void)
     set_string(&str, "This is a program");
     printf("%s\n", str.text);
     printf("%d\n", str.length);
-    printf("%c\n", get_char(&str, 17));
-    free(str.text);
+    printf("%c\n", get_char(&str, 13));
+    destroy_string(&str);
     return 0;
 }
 
 void set_string(string *str, char *text)
 {
-    if (str->text == NULL)
-    {
-        str->length = 0;
-        while (*text != '\0')
-        {
-            str->length++;
-            text++;
-        }
-        text -= str->length;
-        str->text = (char *)malloc((str->length + 1) * sizeof(char));
-        for (int i = 0; i < str->length; i++)
-        {
-            *(str->text) = *text;
-            text++;
-            str->text++;
-        }
-        text -= str->length;
-        *(str->text) = '\0';
-        str->text -= str->length;
-    }
-    else 
+    if (str->text != NULL && text != NULL)
     {
         free(str->text);
         str->length = 0;
@@ -73,6 +55,33 @@ void set_string(string *str, char *text)
         *(str->text) = '\0';
         str->text -= str->length;
     }
+    else if (str->text == NULL && text != NULL)
+    {
+        str->length = 0;
+        while (*text != '\0')
+        {
+            str->length++;
+            text++;
+        }
+        text -= str->length;
+        str->text = (char *)malloc((str->length + 1) * sizeof(char));
+        for (int i = 0; i < str->length; i++)
+        {
+            *(str->text) = *text;
+            text++;
+            str->text++;
+        }
+        text -= str->length;
+        *(str->text) = '\0';
+        str->text -= str->length;
+    }
+    else
+    {
+        printf("--> Failed to set string. Setting string text to null value is not supported by this function <--\n");
+        printf("--> Remember to set newly declared strings to \"default_string\" and call the \"destroy_string\" function when the string is no longer needed. <--\n");
+        printf("--> Note: The \"destroy_string\" function does allow for the string's text to be altered afterwards via the \"set_string\" function. <--\n");
+        exit(1);
+    }
 }
 
 char get_char(string* str, int index) {
@@ -85,7 +94,12 @@ char get_char(string* str, int index) {
         str->text -= index;
         return result;
     } else {
-        printf("--> Index out of bounds error. Tried to access an element beyond the length of the string <--");
+        printf("--> Index out of bounds error. Tried to access an element beyond the length of the string <--\n");
         exit(1);
     }
+}
+
+void destroy_string(string* str) {
+    free(str->text);
+    str->text = NULL;
 }
